5742ddd1fd8bf91d82cdb92ebc125cdfd38e74d5
Veselin Asenov <batveeeees@gmail.com>
2021-04-01 19:12:16 +0100

    Added code to demonstrate the use of a simple for loop.

---
+++ story/repetitions/for_loop/simple,py.py
@@ -0,0 +1,17 @@
+# Task for for loop
+
+print("How many mountains should I display?")
+broi = int(input())
+
+for kolko in range(broi):
+    print('''
+    
+           __
+          /  \_  
+         /^    \\
+        /  ^    \_
+      _/ ^ ^     ^\\
+     /  ^     ^    \\
+
+    
+    ''')
