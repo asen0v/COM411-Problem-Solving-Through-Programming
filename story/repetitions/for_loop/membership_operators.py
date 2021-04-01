a36d62e8b7bd0c69ef2d2bcd3f191e2e1647d9cd
Veselin Asenov <batveeeees@gmail.com>
2021-04-01 20:12:24 +0100

    Added code to demonstrate the use of membership operators with a for loop.

---
+++ story/repetitions/for_loop/membership_operators.py
@@ -0,0 +1,11 @@
+# Task for membership operators
+
+print("What phrase do you see?")
+text = input()
+
+print("Reversing...")
+print("The phrase is: ", end="")
+oburnato = ""
+for bukva in text:
+    oburnato = bukva + oburnato
+print(oburnato)
